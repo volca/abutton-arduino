1e050861ce5963495b1c360034a57145ee670285
volca <volca@tom.com>
2017-03-24 14:14:55 +0800

    migrate to Adafruit_NeoPixel

--- src/src.ino
+++ src/src.ino
@@ -5,32 +5,25 @@
 #include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
 #include <WiFiManager.h>  
 
-#include <NeoPixelBus.h>
+#include <Adafruit_NeoPixel.h>
 
 #define colorSaturation 128
 
-const uint8_t PIN_LED = 5;
+#define LED_PIN         5
+#define NUMPIXELS       1
 
-RgbColor red(colorSaturation, 0, 0);
-RgbColor green(0, colorSaturation, 0);
-RgbColor blue(0, 0, colorSaturation);
-RgbColor white(colorSaturation);
-RgbColor black(0);
-
-HslColor hslRed(red);
-
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
 WiFiManager wifiManager;
-NeoPixelBus<NeoGrbFeature, NeoEsp8266BitBang800KbpsMethod> strip(1, PIN_LED);
 
 void setup() {
     Serial.begin(115200);
     wifiManager.autoConnect("AButton");
-    strip.Begin();
+    pixels.begin();
 }
 
 void loop() {
     Serial.println("loop");
-    strip.SetPixelColor(0, hslRed);
-    strip.Show();
+    pixels.setPixelColor(0, pixels.Color(255,0,0));
+    pixels.show();
     delay(1000);
 }
