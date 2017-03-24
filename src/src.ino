8afb55ae9ba1d6bf41c65813467a5d5a3e29ca98
volca <volca@tom.com>
2017-03-24 15:34:34 +0800

    loadconfig

--- src/src.ino
+++ src/src.ino
@@ -1,4 +1,3 @@
-#include "Arduino.h"
 #include <FS.h>                   //this needs to be first, or it all crashes and burns...
 #include <ESP8266WiFi.h>          //ESP8266 Core WiFi Library (you most likely already have this in your sketch)
 
@@ -16,13 +15,41 @@
 
 const char CONFIG_FILE[] = "config.json";
 
+//define your default values here, if there are different values in config.json, they are overwritten.
+char customUrl[128];
+
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
-WiFiManager wifiManager;
 
+//flag for saving data
+bool shouldSaveConfig = false;
 
+//callback notifying us of the need to save config
+void saveConfigCallback () {
+	Serial.println("Should save config");
+	shouldSaveConfig = true;
+}
 
 void loadConfig() {
+    SPIFFS.begin();
     if (!SPIFFS.exists(CONFIG_FILE)) { 
+        Serial.println("Could not find config file");
+        return;
+    }
+
+    File configFile = SPIFFS.open("/config.json", "r");
+	size_t size = configFile.size();
+	// Allocate a buffer to store contents of the file.
+	std::unique_ptr<char[]> buf(new char[size]);
+
+	configFile.readBytes(buf.get(), size);
+	DynamicJsonBuffer jsonBuffer;
+	JsonObject& json = jsonBuffer.parseObject(buf.get());
+	json.printTo(Serial);
+
+    if (json.success()) {
+        Serial.println("\nparsed json");
+        strcpy(customUrl, json["url"]);
+        Serial.println("Parsed config");
     }
 }
 
@@ -31,13 +58,21 @@ void setup() {
     Serial.println("Loading config");
     loadConfig();
 
+    WiFiManager wifiManager;
+
+	//set config save notify callback
+	wifiManager.setSaveConfigCallback(saveConfigCallback);
+    wifiManager.setAPStaticIPConfig(IPAddress(192,168,1,1), IPAddress(192,168,1,1), IPAddress(255,255,255,0));
+
     //reset settings - for testing
     wifiManager.resetSettings();
 
-    WiFiManagerParameter customUrl("url", "custom url", "", 40, "");
+    // The extra parameters to be configured (can be either global or just in the setup)
+    // After connecting, parameter.getValue() will get you the configured value
+    // id/name placeholder/prompt default length
+    WiFiManagerParameter customUrl("url", "custom url", customUrl, 128);
     wifiManager.addParameter(&customUrl);
 
-    wifiManager.setAPStaticIPConfig(IPAddress(192,168,1,1), IPAddress(192,168,1,1), IPAddress(255,255,255,0));
     wifiManager.autoConnect("AButton");
 
     pixels.begin();
