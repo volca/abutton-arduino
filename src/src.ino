db0bace57109bfa0ef5a4c101d2e54c7a07bb7f6
volca <volca@tom.com>
2017-03-24 15:10:35 +0800

    config

--- src/src.ino
+++ src/src.ino
@@ -1,4 +1,5 @@
 #include "Arduino.h"
+#include <FS.h>                   //this needs to be first, or it all crashes and burns...
 #include <ESP8266WiFi.h>          //ESP8266 Core WiFi Library (you most likely already have this in your sketch)
 
 #include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
@@ -6,18 +7,39 @@
 #include <WiFiManager.h>  
 
 #include <Adafruit_NeoPixel.h>
-
-#define colorSaturation 128
+#include <ESP8266mDNS.h>
+#include <ArduinoOTA.h>
+#include <Ticker.h>
 
 #define LED_PIN         5
 #define NUMPIXELS       1
 
+const char CONFIG_FILE[] = "config.json";
+
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
 WiFiManager wifiManager;
 
+
+
+void loadConfig() {
+    if (!SPIFFS.exists(CONFIG_FILE)) { 
+    }
+}
+
 void setup() {
     Serial.begin(115200);
+    Serial.println("Loading config");
+    loadConfig();
+
+    //reset settings - for testing
+    wifiManager.resetSettings();
+
+    WiFiManagerParameter customUrl("url", "custom url", "", 40, "");
+    wifiManager.addParameter(&customUrl);
+
+    wifiManager.setAPStaticIPConfig(IPAddress(192,168,1,1), IPAddress(192,168,1,1), IPAddress(255,255,255,0));
     wifiManager.autoConnect("AButton");
+
     pixels.begin();
 }
 
